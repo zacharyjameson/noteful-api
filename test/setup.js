1143e84e3dffe2847b28f5d9b0558fb595d68e2a
Zack Jameson <zacharymjameson@gmail.com>
2021-04-16 14:24:30 -0400

    endpoint file

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
-const { expect } = require('chai');
-const supertest = require('supertest');
+process.env.NODE_ENV = "test";
+require("dotenv").config();
+const { expect } = require("chai");
+const supertest = require("supertest");
 
 global.expect = expect;
 global.supertest = supertest;
