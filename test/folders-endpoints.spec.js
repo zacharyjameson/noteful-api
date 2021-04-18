2d2a44f89d856e5e94db1ccad745dc21f8c8865f
Zack Jameson <zacharymjameson@gmail.com>
2021-04-18 10:41:41 -0400

    first 2 tests passing

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -1,7 +1,7 @@
 const knex = require("knex");
 const supertest = require("supertest");
 const app = require("../src/app");
-const { expect } = require("chai");
+const { expect } = require('chai');
 const { makeFoldersArray } = require("./folders.fixtures");
 
 describe("Folders Endpoints", function () {
@@ -17,11 +17,15 @@ describe("Folders Endpoints", function () {
 
   after("disconnect from db", () => db.destroy());
 
-  before("clean the table", () => db("noteful_folders").truncate());
+  before("clean the table", () =>
+    db.raw("TRUNCATE noteful_folders, noteful_notes RESTART IDENTITY CASCADE")
+  );
 
-  afterEach("cleanup", () => db("noteful_folders").truncate());
+  afterEach("cleanup", () =>
+    db.raw("TRUNCATE noteful_folders, noteful_notes RESTART IDENTITY CASCADE")
+  );
 
-  describe.only(`GET /api/folders`, () => {
+  describe(`GET /api/folders`, () => {
     context(`Given no folders`, () => {
       it(`responds with 200 and an empty list`, () => {
         return supertest(app).get("/api/folders").expect(200, []);
