83f468e1b5e3ce69537aea2528800aaaed11ba8a
Zack Jameson <zacharymjameson@gmail.com>
2021-04-18 09:13:08 -0400

    foldersrouter for all folders added

---
+++ src/folders/folders-router.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const FoldersService = require("./folders-service");
+
+const foldersRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeFolder = (folder) => ({
+  id: folder.id,
+  name: xss(folder.name),
+});
+
+foldersRouter
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    FoldersService.getAllFolders(knexInstance)
+      .then((folders) => {
+        res.json(folders.map(serializeFolder));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name } = req.body;
+    const newFolder = { name };
+
+    for (const [key, value] of Object.entries(newArticle))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+
+    FoldersService.addFolder(req.app.get("db"), newFolder)
+      .then((folder) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializeFolder(folder));
+      })
+      .catch(next);
+  });
