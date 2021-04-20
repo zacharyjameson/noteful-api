96d59dba88fe3cb1094165cd7546864d7b5e912a
Zack Jameson <zacharymjameson@gmail.com>
2021-04-20 16:45:11 -0400

    production error fix

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://zacharyjameson@localhost/noteful",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://zacharyjameson@localhost/noteful-test",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://zacharyjameson@localhost/noteful",
 };
