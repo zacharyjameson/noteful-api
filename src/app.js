4e82b4548b11d3680d48dbb026463972c3be3eff
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 08:36:58 -0400

    errorhandler correction

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const { NODE_ENV } = require("./config");
 const foldersRouter = require("./folders/folders-router");
 const noteRouter = require("./notes/notes-router");
-const errorHandler = require("./errorHandler");
+const errorHandler = require("./error-handler");
 
 const app = express();
 
