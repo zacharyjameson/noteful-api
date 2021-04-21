277fa61a8d09d3f667699eed8a80c679704b20ad
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 16:28:43 -0400

    production dburl

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://dunder-mifflin@localhost/noteful",
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    "postgresql://dunder-mifflin@localhost/noteful-test",
+    "postgresql://dunder_mifflin@localhost/noteful-test",
 };
