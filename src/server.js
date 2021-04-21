e32fd0dda6f7b72b7e42c9b8e45ec32cab1fcadf
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 11:55:58 -0400

    ssl

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
-const { PORT, DATABASE_URL } = require("./config");
+const { PORT, DATABASE_URL, SSL } = require("./config");
 const knex = require("knex");
 
 const db = knex({
   client: "pg",
   connection: DATABASE_URL,
+  ssl: SSL,
 });
 
 app.set("db", db);
