95a094a5d2b86014d73ea6621b2bc3aa27f210ec
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 16:57:20 -0400

    script update

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,7 @@ module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString":
-    process.env.NODE_ENV === "test"
+    (process.env.NODE_ENV === "test")
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
   "ssl": { "require": true, "rejectUnauthorized": false },
