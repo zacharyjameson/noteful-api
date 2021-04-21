dac833085fc91855e19e1431c5acabadab0102a0
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 13:54:01 -0400

    normalizing

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://zacharyjameson@localhost/noteful?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+    process.env.DATABASE_URL || "postgresql://dunder-mifflin@localhost/noteful",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://zacharyjameson@localhost/noteful-test",
