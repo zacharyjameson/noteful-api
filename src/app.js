1a1ee3a25d4d1ce96fef8b8ac648a61db5696916
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 08:31:15 -0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const { NODE_ENV } = require("./config");
 const foldersRouter = require("./folders/folders-router");
 const noteRouter = require("./notes/notes-router");
+const errorHandler = require("./errorHandler");
 
 const app = express();
 
@@ -25,15 +26,6 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.log(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
