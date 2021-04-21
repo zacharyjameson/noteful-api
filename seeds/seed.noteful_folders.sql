266785c6cb1b5be196cc63e0318f8a8944f54756
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 19:40:18 -0400

    test seeds

--- seeds/seed.noteful_folders.sql
+++ seeds/seed.noteful_folders.sql
@@ -1,4 +1,5 @@
 INSERT INTO noteful_folders (folder_name)
 VALUES ('The Mines of Moria'),
     ('The Iron Hills'),
-    ('Erebor');
+    ('Erebor'),
+    ('Rohan');
--- seeds/seed.noteful_notes.sql
+++ seeds/seed.noteful_notes.sql
@@ -1,4 +1,5 @@
 INSERT INTO noteful_notes (note_name, folder_id, content)
 VALUES ('Note 1', 1, 'We dug too deep'),
 ('Note 2', 2, 'I will have war'),
-('Note 3', 3, 'I will not part with a single piece of it');
+('Note 3', 3, 'I will not part with a single piece of it'),
+('Note 4', 4, 'Where was Gondor when the Westfold fell?');
