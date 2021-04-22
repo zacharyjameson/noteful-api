37660e41d5d962c57f72c40cff1db30d54252262
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 20:02:07 -0400

    refactoring

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 const knex = require("knex");
-const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const db = knex({
   client: "pg",
