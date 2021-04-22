6b2c4536dea675d2f9ad7d97cce328ed3e5a1a31
Zack Jameson <zacharymjameson@gmail.com>
2021-04-21 20:00:15 -0400

    db url fix

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgres://kyquqxermxrmhz:ed3d7b68c560635b917bf17fce9a22c87b49d6e1563e37a8a4b67330f46f8364@ec2-52-45-73-150.compute-1.amazonaws.com:5432/df64as3tcmtemb",
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/noteful-test",
